607565ccac36a4c8a945a1e77c8d0f6ab6d4e4b0
Troy Hinckley <t.macman@gmail.com>
2023-09-07 15:54:35 -0500

    Cleanup benchmarks

--- rope_benches/src/main.rs
+++ rope_benches/src/main.rs
@@ -1,35 +1,28 @@
-
 // #[macro_use]
 extern crate criterion;
 use criterion::*;
 
 use crdt_testdata::*;
 
-// extern crate rand;
-// use rand::seq::IteratorRandom;
 use rand::prelude::*;
 
 mod rope;
 use self::rope::*;
 use jumprope::*;
 
-mod edittablestr;
-
 use std::cmp::min;
 
-use ropey::Rope as RopeyRope;
-use an_rope::Rope as AnRope;
-use xi_rope::Rope as XiRope;
 use crop::Rope as CropRope;
+use ropey::Rope as RopeyRope;
 
-const CHARS: &[u8; 83] = b" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()[]{}<>?,./";
+const CHARS: &[u8; 83] =
+    b" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()[]{}<>?,./";
 
 // Gross. Find a way to reuse the code from random_unicode_string.
 fn random_ascii_string(rng: &mut SmallRng, len: usize) -> String {
     let mut s = String::new();
     for _ in 0..len {
-        // s.push(*rng.choose(CHARS).unwrap() as char);
-        s.push(CHARS[rng.gen_range(0 .. CHARS.len())] as char);
+        s.push(CHARS[rng.gen_range(0..CHARS.len())] as char);
     }
     s
 }
@@ -38,35 +31,51 @@ impl Rope for JumpRope {
     const NAME: &'static str = "JumpRope";
 
     #[inline(always)]
-    fn new() -> Self { JumpRope::new() }
-
-    #[inline(always)]
-    fn insert_at(&mut self, pos: usize, contents: &str) { self.insert(pos, contents); }
-    #[inline(always)]
-    fn del_at(&mut self, pos: usize, len: usize) { self.remove(pos..pos+len); }
-
-    #[inline(always)]
-    fn edit_at(&mut self, pos: usize, del_len: usize, ins_content: &str) {
-        self.replace(pos..pos+del_len, ins_content);
+    fn new() -> Self {
+        JumpRope::new()
     }
 
     #[inline(always)]
-    fn to_string(&self) -> String { ToString::to_string(self) }
+    fn insert_at(&mut self, pos: usize, contents: &str) {
+        self.insert(pos, contents);
+    }
+    #[inline(always)]
+    fn del_at(&mut self, pos: usize, len: usize) {
+        self.remove(pos..pos + len);
+    }
 
     #[inline(always)]
-    fn char_len(&self) -> usize { self.len_chars() } // in unicode values
+    fn edit_at(&mut self, pos: usize, del_len: usize, ins_content: &str) {
+        self.replace(pos..pos + del_len, ins_content);
+    }
+
+    #[inline(always)]
+    fn to_string(&self) -> String {
+        ToString::to_string(self)
+    }
+
+    #[inline(always)]
+    fn char_len(&self) -> usize {
+        self.len_chars()
+    } // in unicode values
 }
 
 impl Rope for JumpRopeBuf {
     const NAME: &'static str = "JumpRopeBuf";
 
     #[inline(always)]
-    fn new() -> Self { JumpRopeBuf::new() }
+    fn new() -> Self {
+        JumpRopeBuf::new()
+    }
 
     #[inline(always)]
-    fn insert_at(&mut self, pos: usize, contents: &str) { self.insert(pos, contents); }
+    fn insert_at(&mut self, pos: usize, contents: &str) {
+        self.insert(pos, contents);
+    }
     #[inline(always)]
-    fn del_at(&mut self, pos: usize, len: usize) { self.remove(pos..pos+len); }
+    fn del_at(&mut self, pos: usize, len: usize) {
+        self.remove(pos..pos + len);
+    }
 
     #[inline(always)]
     fn edit_at(&mut self, pos: usize, del_len: usize, ins_content: &str) {
@@ -79,61 +88,13 @@ impl Rope for JumpRopeBuf {
     }
 
     #[inline(always)]
-    fn to_string(&self) -> String { ToString::to_string(self) }
-
-    #[inline(always)]
-    fn char_len(&self) -> usize { self.len_chars() } // in unicode values
-}
-
-impl Rope for AnRope {
-    const NAME: &'static str = "AnRope";
-
-    #[inline(always)]
-    fn new() -> Self { AnRope::new() }
-
-    #[inline(always)]
-    fn insert_at(&mut self, pos: usize, contents: &str) { *self = self.insert_str(pos, contents); }
-    #[inline(always)]
-    fn del_at(&mut self, pos: usize, len: usize) { *self = self.delete(pos..pos+len); }
-
-    #[inline(always)]
-    fn to_string(&self) -> String { ToString::to_string(self) }
-
-    #[inline(always)]
-    fn char_len(&self) -> usize { self.len() } // in unicode values
-}
-
-impl Rope for XiRope {
-    const NAME: &'static str = "XiRope";
-
-    #[inline(always)]
-    fn new() -> Self { XiRope::from("") }
-
-    #[inline(always)]
-    fn insert_at(&mut self, pos: usize, contents: &str) {
-        self.edit(pos..pos, contents);
-    }
-    #[inline(always)]
-    fn del_at(&mut self, pos: usize, len: usize) {
-        self.edit(pos..pos+len, "");
-    }
-    #[inline(always)]
-    fn edit_at(&mut self, pos: usize, del_len: usize, ins_content: &str) {
-        self.edit(pos..pos+del_len, ins_content);
-    }
-
-    #[inline(always)]
     fn to_string(&self) -> String {
-        String::from(self)
+        ToString::to_string(self)
     }
 
     #[inline(always)]
     fn char_len(&self) -> usize {
-        let mut len = 0;
-        for s in self.iter_chunks(..) {
-            len += s.chars().count();
-        }
-        len
+        self.len_chars()
     } // in unicode values
 }
 
@@ -141,7 +102,9 @@ impl Rope for RopeyRope {
     const NAME: &'static str = "Ropey";
 
     #[inline(always)]
-    fn new() -> Self { RopeyRope::new() }
+    fn new() -> Self {
+        RopeyRope::new()
+    }
 
     #[inline(always)]
     fn insert_at(&mut self, pos: usize, contents: &str) {
@@ -149,17 +112,18 @@ impl Rope for RopeyRope {
     }
     #[inline(always)]
     fn del_at(&mut self, pos: usize, len: usize) {
-        self.remove(pos..pos+len);
+        self.remove(pos..pos + len);
     }
-    // fn del_at<R: RangeBounds<usize>>(&mut self, range: R);
-
-    // fn slice(&self, pos: usize, len: usize) -> Result<String, RopeError>;
 
     #[inline(always)]
-    fn to_string(&self) -> String { unimplemented!() }
+    fn to_string(&self) -> String {
+        unimplemented!()
+    }
 
     #[inline(always)]
-    fn char_len(&self) -> usize { self.len_chars() } // in unicode values
+    fn char_len(&self) -> usize {
+        self.len_chars()
+    } // in unicode values
 }
 
 impl Rope for CropRope {
@@ -175,7 +139,7 @@ impl Rope for CropRope {
     }
 
     fn del_at(&mut self, pos: usize, len: usize) {
-        self.delete(pos..pos+len)
+        self.delete(pos..pos + len)
     }
 
     fn to_string(&self) -> String {
@@ -187,73 +151,14 @@ impl Rope for CropRope {
     }
 }
 
-use std::os::raw::c_char;
-use std::ffi::CString;
 use crdt_testdata::{load_testing_data, TestData};
 use criterion::measurement::WallTime;
-
-#[repr(C)]
-struct CRopeRaw { _unused : [ u8 ; 0 ] }
-
-extern {
-    fn rope_new() -> *mut CRopeRaw;
-    fn rope_new_with_utf8(s: *const c_char) -> *mut CRopeRaw;
-    fn rope_free(r: *mut CRopeRaw);
-    fn rope_char_count(r: *const CRopeRaw) -> usize;
-    // fn rope_byte_count(r: *const CRopeRaw) -> usize;
-
-    fn rope_insert(r: *mut CRopeRaw, pos: usize, s: *const c_char) -> u32;
-    fn rope_del(r: *mut CRopeRaw, pos: usize, len: usize) -> u32;
-}
-
-struct CRope(*mut CRopeRaw);
-impl Rope for CRope {
-    const NAME: &'static str = "C-JumpRope";
-
-    #[inline(always)]
-    fn new() -> Self { unsafe { CRope(rope_new()) } }
-
-    #[inline(always)]
-    fn insert_at(&mut self, pos: usize, contents: &str) {
-        unsafe {
-            let cstr = CString::new(contents).unwrap();
-            rope_insert(self.0, pos, cstr.as_ptr());
-        }
-    }
-    #[inline(always)]
-    fn del_at(&mut self, pos: usize, len: usize) {
-        unsafe { rope_del(self.0, pos, len); }
-    }
-    fn to_string(&self) -> String { unimplemented!() }
-
-    #[inline(always)]
-    fn char_len(&self) -> usize { unsafe { rope_char_count(self.0) } } // in unicode values
-}
-impl Drop for CRope {
-    fn drop(&mut self) {
-        unsafe { rope_free(self.0); }
-    }
-}
-impl From<String> for CRope {
-    fn from(s: String) -> Self {
-        let cstr = CString::new(s).unwrap();
-        CRope(unsafe { rope_new_with_utf8(cstr.as_ptr()) })
-    }
-}
-
-#[test]
-fn foo() {
-    unsafe {
-        let r = rope_new();
-        println!("size {}", rope_char_count(r));
-    }
-}
 
 fn gen_strings(rng: &mut SmallRng) -> Vec<String> {
     // I wish there was a better syntax for just making an array here.
     let mut strings = Vec::<String>::new();
     for _ in 0..100 {
-        let len = rng.gen_range(1 .. 3);
+        let len = rng.gen_range(1..3);
         strings.push(random_ascii_string(rng, len));
     }
 
@@ -268,7 +173,7 @@ fn ins_append<R: Rope>(b: &mut Bencher) {
     let mut len = 0;
     b.iter(|| {
         // let pos = rng.gen_range(0, len+1);
-        let text = &strings[rng.gen_range(0 .. strings.len())];
+        let text = &strings[rng.gen_range(0..strings.len())];
         r.insert_at(len, text.as_str());
         len += text.chars().count();
     });
@@ -284,14 +189,14 @@ fn ins_random<R: Rope>(b: &mut Bencher) {
     // Len isn't needed, but its here to allow direct comparison with ins_append.
     let mut len = 0;
     b.iter(|| {
-        let pos = rng.gen_range(0 .. len+1);
-        let text = &strings[rng.gen_range(0 .. strings.len())];
+        let pos = rng.gen_range(0..len + 1);
+        let text = &strings[rng.gen_range(0..strings.len())];
         r.insert_at(pos, text.as_str());
         len += text.chars().count();
     });
 
     black_box(r.char_len());
-    black_box(len); 
+    black_box(len);
 }
 
 fn stable_ins_del<R: Rope + From<String>>(b: &mut Bencher, target_length: &u64) {
@@ -300,30 +205,21 @@ fn stable_ins_del<R: Rope + From<String>>(b: &mut Bencher, target_length: &u64) 
 
     // I wish there was a better syntax for just making an array here.
     let strings = gen_strings(&mut rng);
-    
-    // let target_length = 100000;
-    // let mut r = R::new();
-    // while r.char_len() < target_length {
-    //     // The rope should be a hot mess.
-    //     let pos = rng.gen_range(0, r.char_len()+1);
-    //     r.insert_at(pos, strings[rng.gen_range(0, strings.len())].as_str()).unwrap();
-    // }
+
     let mut r = R::from(random_ascii_string(&mut rng, target_length));
     let mut len = target_length;
 
     b.iter(|| {
-        // let len = r.char_len();
-        // if len == 0 || rng.gen::<bool>() {
         if len <= target_length {
             // Insert
-            let pos = rng.gen_range(0 .. len+1);
-            let text = &strings[rng.gen_range(0 .. strings.len())];
+            let pos = rng.gen_range(0..len + 1);
+            let text = &strings[rng.gen_range(0..strings.len())];
             r.insert_at(pos, text.as_str());
             len += text.chars().count();
         } else {
             // Delete
-            let pos = rng.gen_range(0 .. len);
-            let dlen = min(rng.gen_range(0 .. 10), len - pos);
+            let pos = rng.gen_range(0..len);
+            let dlen = min(rng.gen_range(0..10), len - pos);
             len -= dlen;
 
             r.del_at(pos, dlen);
@@ -343,10 +239,7 @@ fn bench_ins_append(c: &mut Criterion) {
 
     group.bench_function("jumprope", ins_append::<JumpRope>);
     group.bench_function("ropey", ins_append::<RopeyRope>);
-    // group.bench_function("anrope", ins_append::<AnRope>);
-    group.bench_function("xirope", ins_append::<XiRope>);
-    group.bench_function("jumprope_c", ins_append::<CRope>);
-    group.bench_function("raw_string", ins_append::<String>);
+    group.bench_function("crop", ins_append::<CropRope>);
     group.finish();
 }
 
@@ -356,10 +249,7 @@ fn bench_ins_random(c: &mut Criterion) {
 
     group.bench_function("jumprope", ins_random::<JumpRope>);
     group.bench_function("ropey", ins_random::<RopeyRope>);
-    // group.bench_function("anrope", ins_random::<AnRope>);
-    group.bench_function("xirope", ins_random::<XiRope>);
-    group.bench_function("jumprope_c", ins_random::<CRope>);
-    group.bench_function("raw_string", ins_random::<String>);
+    group.bench_function("crop", ins_random::<CropRope>);
     group.finish();
 }
 
@@ -369,22 +259,41 @@ fn bench_stable_ins_del(c: &mut Criterion) {
 
     for size in [1000, 10000, 100000, 1000000, 10000000].iter() {
         group.throughput(Throughput::Elements(*size));
-        group.bench_with_input(BenchmarkId::new("jumprope", size), size, stable_ins_del::<JumpRope>);
-        group.bench_with_input(BenchmarkId::new("ropey", size), size, stable_ins_del::<RopeyRope>);
-        // group.bench_with_input(BenchmarkId::new("anrope", size), size, stable_ins_del::<AnRope>);
-        group.bench_with_input(BenchmarkId::new("xirope", size), size, stable_ins_del::<XiRope>);
-        group.bench_with_input(BenchmarkId::new("jumprope_c", size), size, stable_ins_del::<CRope>);
+        group.bench_with_input(
+            BenchmarkId::new("jumprope", size),
+            size,
+            stable_ins_del::<JumpRope>,
+        );
+        group.bench_with_input(
+            BenchmarkId::new("ropey", size),
+            size,
+            stable_ins_del::<RopeyRope>,
+        );
+        group.bench_with_input(
+            BenchmarkId::new("crop", size),
+            size,
+            stable_ins_del::<CropRope>,
+        );
     }
     group.finish();
 }
 
 fn load_named_data(name: &str) -> TestData {
-    let filename = format!("/home/seph/src/diamond-types/benchmark_data/{}.json.gz", name);
+    let filename = format!(
+        "{}/../benchmark_data/{}.json.gz",
+        env!("CARGO_MANIFEST_DIR"),
+        name
+    );
+    println!("loading {}", filename);
     load_testing_data(&filename)
 }
 
-// const DATASETS: &[&str] = &["automerge-paper"];
-const DATASETS: &[&str] = &["automerge-paper", "rustcode", "sveltecomponent", "seph-blog1"];
+const DATASETS: &[&str] = &[
+    "automerge-paper",
+    "rustcode",
+    "sveltecomponent",
+    "seph-blog1",
+];
 
 fn realworld(c: &mut Criterion) {
     for name in DATASETS {
@@ -392,13 +301,6 @@ fn realworld(c: &mut Criterion) {
         let test_data_chars = load_named_data(name);
         group.throughput(Throughput::Elements(test_data_chars.len() as u64));
         let test_data_bytes = test_data_chars.chars_to_bytes();
-
-        let mut all_ascii = true;
-        for txn in &test_data_chars.txns {
-            for TestPatch(_pos, _del, ins) in &txn.patches {
-                if ins.chars().count() != ins.len() { all_ascii = false; }
-            }
-        }
 
         fn x<R: Rope>(group: &mut BenchmarkGroup<WallTime>, name: &str, test_data: &TestData) {
             assert_eq!(R::EDITS_USE_BYTE_OFFSETS, test_data.using_byte_positions);
@@ -420,17 +322,7 @@ fn realworld(c: &mut Criterion) {
         x::<RopeyRope>(&mut group, name, &test_data_chars);
         x::<JumpRope>(&mut group, name, &test_data_chars);
         x::<JumpRopeBuf>(&mut group, name, &test_data_chars);
-        x::<CRope>(&mut group, name, &test_data_chars);
         x::<CropRope>(&mut group, name, &test_data_bytes);
-
-        // These two crash on non-ascii characters for some reason.
-        if all_ascii {
-            // Extremely slow.
-            x::<XiRope>(&mut group, name, &test_data_chars);
-
-            // Crashes.
-            // x::<AnRope>(&mut group, name, &test_data);
-        }
 
         // This takes a long time to run.
         // x::<String>(&mut group, name, &test_data);
@@ -439,7 +331,8 @@ fn realworld(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches,
+criterion_group!(
+    benches,
     bench_ins_append,
     bench_ins_random,
     bench_stable_ins_del,
